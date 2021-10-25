e2342a286be0445d0062d9cb444ad19b989c7988
Mihael Konjevic <konjevic@gmail.com>
2021-10-26 00:16:36 +0200

    Proxy meta state

--- src/app/controllers/proxy.cljs
+++ src/app/controllers/proxy.cljs
@@ -9,9 +9,12 @@
       (core/get-api* controller-name)
       deref))
 
-(defmethod ctrl/init ::controller [{::keys [app controller-name] :keys [state*] :as ctrl}]
-  (let [unsubscribe (core/subscribe app controller-name (fn [val] (ctrl/transact ctrl #(reset! state* val))))]
-    (assoc ctrl ::unsubscribe unsubscribe)))
+(defmethod ctrl/init ::controller [{::keys [app controller-name] :keys [state* meta-state*] :as ctrl}]
+  (let [unsubscribe (core/subscribe app controller-name (fn [val] (ctrl/transact ctrl #(reset! state* val))))
+        unsubscribe-meta (core/subscribe-meta app controller-name (fn [val] (ctrl/transact ctrl #(reset! meta-state* val))))]
+    (assoc ctrl
+           ::unsubscribe unsubscribe
+           ::unsubscribe-meta unsubscribe-meta)))
 
 (defmethod ctrl/start ::controller [{::keys [app controller-name]} _ _ _]
   (core/get-derived-state app controller-name))
@@ -21,8 +24,9 @@
     (binding [core/*transaction-depth* 0]
       (core/dispatch app controller-name ev payload))))
 
-(defmethod ctrl/terminate ::controller [{::keys [unsubscribe]}]
-  (unsubscribe))
+(defmethod ctrl/terminate ::controller [{::keys [unsubscribe unsubscribe-meta]}]
+  (unsubscribe)
+  (unsubscribe-meta))
 
 (defn make [app controller-name]
   {:keechma.controller/params true
